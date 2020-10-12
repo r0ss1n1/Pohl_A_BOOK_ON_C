31700ef1d41d9fccce15eefa5723a98ff35017d6
r0ss1n1 <43396895+r0ss1n1@users.noreply.github.com>
2020-10-13 00:22:13 +1100

    Add files via upload

---
+++ capitalize.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+	int c;
+	while((c = getchar()) != EOF){
+
+		if (c >= 'a' && c <= 'z') {
+			putchar(c + 'A' - 'a');
+		} else {
+			putchar(c);
+
+		}
+	}
+
+	return 0;
+}
---
+++ power_square.c
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void) {
+
+	double x;
+	printf("\n%s","The following will be computed:\n\n   the square root of x\n   x raised to the power x\n\n");
+
+	while(1){
+		printf("Input x: ");
+		if (scanf("%lf", &x) != 1) break;
+		if (x >= 0.0) {
+			printf("\n%15s%22.15e\n%15s%22.15e\n%15s%22.15e\n\n", "x = ", x, "sqrt(x) = ", sqrt(x), "pow(x, x) = ", pow(x, x));
+
+		} else {
+			printf("\nSorry, your number must be nonnegative.\n");
+			break;
+
+		}
+	}
+	printf("\nBye!\n\n");
+
+	return 0;
+
+}
